958b488cd24031d9aed26d4d5ad445726d646534
GetPageSpeed Builder <info@getpagespeed.com>
2018-05-10 15:44:58 +0000

    Test

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf *.rpm
-spectool -g wrk.spec
-mock -r epel-7-x86_64 --spec=wrk.spec --sources=. --resultdir=. --buildsrpm
-mock -r epel-7-x86_64 --rebuild --resultdir=. *.src.rpm 
